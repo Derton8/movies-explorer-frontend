a6927d356c7f9ee82df2e61e493e2951666c8f75
Danil Mysenko <danil.mysenko@yandex.ru>
2023-05-10 19:26:18 +0300

    Добавить секцию Techs

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,14 @@
 import AboutProject from './AboutProject/AboutProject';
 import './Main.scss';
 import Promo from './Promo/Promo';
+import Techs from './Techs/Techs';
 
 export default function Main(props) {
   return (
     <main className='main'>
       <Promo></Promo>
       <AboutProject></AboutProject>
+      <Techs></Techs>
     </main>
   );
 }
