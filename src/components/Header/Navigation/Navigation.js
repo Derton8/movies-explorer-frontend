064c8f3eb75f0c886bd128be1ba6b33b4d7d4d98
Danil Mysenko <danil.mysenko@yandex.ru>
2023-05-18 18:54:18 +0300

    feat(navigation): Реализовать бургер-меню

--- src/components/Header/Navigation/Navigation.js
+++ src/components/Header/Navigation/Navigation.js
@@ -21,7 +21,43 @@ export default function Navigation(props) {
           </NavLink>
         </div>
       </nav>
-      <button className='navigation__button'></button>
+      <input
+        className='navigation__toggle'
+        id='navigation-toggle'
+        type='checkbox'
+      />
+      <label className='navigation__btn' for='navigation-toggle'>
+        <span></span>
+      </label>
+      <div className='navigation__popup'></div>
+      <ul className='navigation__menu'>
+        <div>
+          <li className='navigation__menu-item'>
+            <NavLink className='navigation__link' to='/'>
+              Главная
+            </NavLink>
+          </li>
+          <li className='navigation__menu-item'>
+            <NavLink className='navigation__link' to='/movies'>
+              Фильмы
+            </NavLink>
+          </li>
+          <li className='navigation__menu-item'>
+            <NavLink className='navigation__link' to='/saved-movies'>
+              Сохранённые фильмы
+            </NavLink>
+          </li>
+        </div>
+        <li className='navigation__menu-item'>
+          <NavLink
+            className='navigation__link navigation__link_type_btn'
+            to='/profile'
+          >
+            Аккаунт
+            <div className='nav__icon'></div>
+          </NavLink>
+        </li>
+      </ul>
     </div>
   );
 }
