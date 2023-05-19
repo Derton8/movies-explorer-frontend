3f77a84911403c1ce1082c9661bb96faafbbe450
Danil Mysenko <danil.mysenko@yandex.ru>
2023-05-19 17:56:20 +0300

    fix: input required

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -4,14 +4,19 @@ export default function SearchForm(props) {
   return (
     <form className='search'>
       <div className='search__columns'>
-        <input className='search__input' type='search' placeholder='Фильм' />
+        <input
+          className='search__input'
+          type='search'
+          placeholder='Фильм'
+          required
+        />
         <button className='search__btn' type='submit'>
           Найти
         </button>
       </div>
       <div className='search__container'>
         <label className='search__switch'>
-          <input type='checkbox' />
+          <input type='checkbox' required />
           <span className='search__slider'></span>
         </label>
         <p className='search__text'>Короткометражки</p>
